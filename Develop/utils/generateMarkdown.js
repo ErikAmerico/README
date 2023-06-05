8bc4d7a801366c019a665c08bf82babe8fc2dc59
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-06-04 21:53:28 -0400

    added function to render license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -30,7 +30,24 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'MIT':
+      return `## License
+      
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).`;
+    case 'Apache 2.0':
+      return `## License
+      
+This project is licensed under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0).`;
+    case 'Mozilla 2.0':
+      return `## License
+      
+This project is licensed under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0).`;
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
