58539322c1179856707e0d3dd95ff41b745a9b21
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-06-04 21:52:17 -0400

    added function that returns license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,7 +15,18 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+    case 'Mozilla 2.0':
+      return '[Mozilla 2.0](https://opensource.org/licenses/MPL-2.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
