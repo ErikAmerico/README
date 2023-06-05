c7ce7423f1ff9dcc4bba900f0ffcb72df1906a37
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-06-04 21:48:09 -0400

    installed inquirer. Created questions within inquirer prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions for your project:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage information for your project:'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Provide contribution guidelines for your project:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide test instructions for your project:',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your project:',
+            choices: ['MIT', 'Apache 2.0', 'Mozilla 2.0', 'None'],
+            default: 'None',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Provide your Github username:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Provide your Email:',
+        },
+    ])
+    .then((answers) => {
+        const READMECONTENT = generateMarkdown(answers);
+
+        fs.writeFile('README.md', READMECONTENT, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md!')
+        );
+    });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
