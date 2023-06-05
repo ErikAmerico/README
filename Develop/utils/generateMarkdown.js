2631449167edc6de32f6d630ac756313e8d4e55b
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-06-04 21:55:47 -0400

    added function to generate readME content

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -51,8 +51,38 @@ This project is licensed under the [Mozilla Public License 2.0](https://opensour
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+## Description
+${data.description}
 
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to reach out:
+
+GitHub: [${data.github}](https://github.com/${data.github})
+Email: ${data.email}
 `;
 }
 
